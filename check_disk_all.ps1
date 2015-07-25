8db166790d982122ed82ecea606849ef286f2b8b
Allyn Stott <allynstott@gmail.com>
2015-07-25 14:25:32 -0700

    Ignore all unnamed volumes as these are generally don't need to be monitored.

--- check_disk_all.ps1
+++ check_disk_all.ps1
@@ -2,7 +2,7 @@ param([int] $warn = 20, [int] $critical = 10)
 
 . (Join-Path $PSScriptRoot checks_helper.ps1)
 
-$countervaluearray = (Get-counter  -Counter "\LogicalDisk(*)\% Free Space").countersamples | where {$_.InstanceName -ne "_total"} | Sort-Object InstanceName
+$countervaluearray = (Get-counter  -Counter "\LogicalDisk(*)\% Free Space").countersamples | where {$_.InstanceName -ne "_total" -and $_.InstanceName -notmatch "harddiskvolume"} | Sort-Object InstanceName
 
 $minvalue = 100
 $message = ""
